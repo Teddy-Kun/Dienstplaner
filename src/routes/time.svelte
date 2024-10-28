6135c1fbc6c4ebde05c861febb581fbc9dc83882
Paul Orzel <pb.orzel@proton.me>
2024-10-28 22:07:42 +0100

    add do nothing save button to time footer

--- src/routes/time.svelte
+++ src/routes/time.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+import { Button } from "$lib/components/ui/button/index";
 import * as Card from "$lib/components/ui/card/index";
+import Save from "~icons/material-symbols/save";
+import { WIP } from "../utils";
+
+function save() {
+    WIP();
+}
 </script>
 
 <Card.Root>
 	<Card.Content>
 		TODO
 	</Card.Content>
+    <Card.Footer class="justify-end">
+		<Button onclick={save}>
+			<Save />
+		</Button>
+	</Card.Footer>
 </Card.Root>
