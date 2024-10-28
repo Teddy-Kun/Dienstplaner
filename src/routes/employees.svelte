562610a26eab6a0197a1f375884804cf2857e27c
Paul Orzel <pb.orzel@proton.me>
2024-10-28 21:32:48 +0100

    implement basic saving and deleting

--- src/routes/employees.svelte
+++ src/routes/employees.svelte
@@ -3,39 +3,81 @@ import * as Table from "$lib/components/ui/table";
 import { Button } from "@lib/components/ui/button/index";
 import * as Card from "@lib/components/ui/card/index";
 import { Checkbox } from "@lib/components/ui/checkbox/index";
+import Input from "@lib/components/ui/input/input.svelte";
+import { onMount } from "svelte";
 import EditIcon from "~icons/material-symbols/edit-square";
 import Save from "~icons/material-symbols/save";
 import TrashIcon from "~icons/mdi/trash-can";
+import { create_employee, delete_employee, get_employees } from "../api";
 import { type Employee, WIP } from "../utils";
 
-let employees: Employee[] = $state([
+interface EditableEmployee extends Employee {
+	checked: boolean;
+}
+let employees: EditableEmployee[] = $state([
 	{
 		id: 1,
 		name: "Max Mustermann",
 		hours: 40,
 		overtime: 0,
+		checked: false,
 	},
 ]);
+
+let newEmployees: EditableEmployee[] = $state([]);
 
 function edit() {
 	WIP();
 }
 
-function deleteEmployee() {
-	WIP();
+function deleteEmployee(id: number) {
+	delete_employee(id).then(() => getEmployees());
 }
 
 function deleteChecked() {
-	WIP();
+	const promises: Promise<unknown>[] = [];
+	for (const employee of employees.filter((employee) => employee.checked))
+		promises.push(delete_employee(employee.id));
+
+	newEmployees = newEmployees.filter((employee) => !employee.checked);
+
+	Promise.allSettled(promises).then(getEmployees);
 }
 
 function add() {
-	WIP();
+	newEmployees.push({
+		id: (newEmployees.length + 1) * -1,
+		name: "",
+		hours: 0,
+		overtime: 0,
+		checked: false,
+	});
 }
 
 function save() {
+	const promises: Promise<unknown>[] = [];
+
+	for (const employee of newEmployees)
+		promises.push(
+			create_employee(employee.name, employee.hours, employee.overtime),
+		);
+
+	Promise.allSettled(promises).then(() => {
+		newEmployees = [];
+		getEmployees();
+	});
+
 	WIP();
 }
+
+async function getEmployees() {
+	employees = (await get_employees()).map((employee) => ({
+		...employee,
+		checked: false,
+	}));
+}
+
+onMount(getEmployees);
 </script>
 
 <Card.Root>
@@ -56,7 +98,7 @@ function save() {
 				{#each employees as employee}
 					<Table.Row>
 						<Table.Cell>
-							<Checkbox />
+							<Checkbox bind:checked={employee.checked} />
 						</Table.Cell>
 						<Table.Cell>{employee.name}</Table.Cell>
 						<Table.Cell>{employee.hours}</Table.Cell>
@@ -65,7 +107,31 @@ function save() {
 							<Button class="mr-1" onclick={edit}>
 								<EditIcon style="width: 20px; height: 20px; path: currentColor" />
 							</Button>
-							<Button onclick={deleteEmployee}>
+							<Button onclick={() => deleteEmployee(employee.id)}>
+								<TrashIcon />
+							</Button>
+						</Table.Cell>
+					</Table.Row>
+				{/each}
+				{#each newEmployees as employee}
+					<Table.Row>
+						<Table.Cell>
+							<Checkbox bind:checked={employee.checked} />
+						</Table.Cell>
+						<Table.Cell>
+							<Input bind:value={employee.name} />
+						</Table.Cell>
+						<Table.Cell>
+							<Input bind:value={employee.hours} type="number" min="0" max="168" />
+						</Table.Cell>
+						<Table.Cell>
+							<Input bind:value={employee.overtime} type="number" />
+						</Table.Cell>
+						<Table.Cell class="flex justify-end items-center">
+							<Button class="mr-1" onclick={edit}>
+								<EditIcon style="width: 20px; height: 20px; path: currentColor" />
+							</Button>
+							<Button onclick={() => deleteEmployee(employee.id)}>
 								<TrashIcon />
 							</Button>
 						</Table.Cell>
